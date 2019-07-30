edb6ae5f96b455b256a1e715727f402b57edb33b
pivovarit <gpiwowarek@gmail.com>
2019-07-30 10:59:49 +0200

    Example mock mvc test

--- movie-details/src/test/java/com/bbh/details/moviedetails/MockMvcMovieDetailsTests.java
+++ movie-details/src/test/java/com/bbh/details/moviedetails/MockMvcMovieDetailsTests.java
@@ -1,28 +1,24 @@
 package com.bbh.details.moviedetails;
 
-import com.bbh.details.moviedetails.web.MovieDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static java.lang.String.format;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 public class MockMvcMovieDetailsTests {
+
+
 
     @Autowired
     private MockMvc mockMvc;
