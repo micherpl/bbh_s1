005a0b8af66dd18843bf54fdf0435bcccf59b4eb
pivovarit <gpiwowarek@gmail.com>
2019-07-30 10:17:31 +0200

    Add simple test

--- movie-details/src/test/java/com/bbh/details/moviedetails/MovieDetailsApplicationTests.java
+++ movie-details/src/test/java/com/bbh/details/moviedetails/MovieDetailsApplicationTests.java
@@ -1,7 +1,6 @@
 package com.bbh.details.moviedetails;
-import com.bbh.details.moviedetails.web.MovieDetails;
 
-import org.assertj.core.api.Assertions;
+import com.bbh.details.moviedetails.web.MovieDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,45 +9,37 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.client.RestTemplate;
 
 import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class MovieDetailsApplicationTests {
 
-	@LocalServerPort
-	private int port;
+    @LocalServerPort
+    private int port;
 
-	@Autowired
-	private TestRestTemplate testRestTemplate;
+    @Autowired
+    private TestRestTemplate testRestTemplate;
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
-	@Test
-	public void shouldPing() {
-		ResponseEntity<String> responseEntity = testRestTemplate
-		.getForEntity(format("http://localhost:%d/ping", port), String.class);
-	}
+    @Test
+    public void shouldPing() {
+        ResponseEntity<String> responseEntity = testRestTemplate
+          .getForEntity(format("http://localhost:%d/ping", port), String.class);
+    }
 
-	@Test
-	public void detailsEndpointReturnsDetails() {
+    @Test
+    public void detailsEndpointReturnsDetails() {
+        ResponseEntity<MovieDetails> responseEntity = testRestTemplate
+          .getForEntity(format("http://localhost:%d/api/details/42", port), MovieDetails.class);
 
-		ResponseEntity<MovieDetails> responseEntity = testRestTemplate
-		.getForEntity(format("http://localhost:%d/api/details/42", port), MovieDetails.class);
-
-		MovieDetails forObject = testRestTemplate
-		.getForObject(format("http://localhost:%d/api/details/42", port), MovieDetails.class);
-
-		MovieDetails forObject2 = testRestTemplate
-		.getForObject(format("http://localhost:%d/api/details/42", port), MovieDetails.class);
-
-		System.out.println(forObject.equals(forObject2));
-		Assertions.assertThat(forObject).isEqualToComparingFieldByField(forObject2);
-		System.out.println(responseEntity.getBody());
-	}
-
+        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        assertThat(responseEntity.getBody().getDetails())
+          .contains("Details");
+    }
 }
