184a2211fd2a73dec43aa9db54f2d4328ea6bf55
pivovarit <gpiwowarek@gmail.com>
2019-07-30 10:35:43 +0200

    Mockito

--- movie-details/src/test/java/com/bbh/details/moviedetails/MovieDetailsApplicationTests.java
+++ movie-details/src/test/java/com/bbh/details/moviedetails/MovieDetailsApplicationTests.java
@@ -1,14 +1,10 @@
 package com.bbh.details.moviedetails;
 
-import com.bbh.details.moviedetails.web.MailSender;
 import com.bbh.details.moviedetails.web.MovieDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
