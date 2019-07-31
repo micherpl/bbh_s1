257833925059606cf98da1ce380720b00e204549
pivovarit <gpiwowarek@gmail.com>
2019-07-31 09:47:20 +0200

    Fix

--- rental-store/src/test/java/com/pivovarit/movies/domain/SpringBootIntegrationTest.java
+++ rental-store/src/test/java/com/pivovarit/movies/domain/SpringBootIntegrationTest.java
@@ -10,6 +10,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpringBootIntegrationTest {
@@ -21,9 +25,8 @@ public class SpringBootIntegrationTest {
     private int port;
 
     @Test
-    public void name() {
-        MovieWithDetailsDto movie = movieFacade.getMovie(42L).get();
-
-        Assertions.assertThat(movie).isNull();
+    public void shouldNotReturnAMovie() {
+        Optional<MovieWithDetailsDto> movie = movieFacade.getMovie(42L);
+        assertThat(movie).isEmpty();
     }
 }
